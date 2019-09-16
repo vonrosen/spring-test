de05883b10c425b8acd68759183059f1e1c82373
Hunter Stern <vonrosen2000@yahoo.com>
2019-09-15 18:49:46 -0700

    more changes

--- src/main/java/org/hunter/springboot/user/UserController.java
+++ src/main/java/org/hunter/springboot/user/UserController.java
@@ -2,6 +2,8 @@ package org.hunter.springboot.user;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    private static final Logger logger = LogManager.getLogger();
+
     @Autowired
     UserRepository userRepository;
 
     @RequestMapping("/users")
     public List<Users> getUsers() {
-        return userRepository.getAllUsers();
+        logger.info("Calling getUsers!");
+        List<Users> users = userRepository.getAllUsers();
+        logger.debug(users.size() + " users returned.");
+        return users;
     }
 
 }
